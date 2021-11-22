15ec2f66461acc38ecedcbdcd86ff7d7bdf212bf
Rafa Mossetto <mossettorafael@gmail.com>
2021-11-22 11:09:46 -0300

    init

--- index.js
+++ index.js
@@ -4,6 +4,4 @@ const server = require('./app');
 
 const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, () => {
-    console.log(`Server listen on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server listen on port ${PORT}`));
